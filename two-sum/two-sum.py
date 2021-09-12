999520401f604eca56f71942a71b782a5a0e9f17
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-09-13 01:15:25 +0800

    Explanations added

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,3 +1,18 @@
+"""
+Time & Space: O(n)
+
+We generate a hash map to store the (ith index, ith element)
+
+Why get the difference of target and ith element?
+The result of the difference of target and ith element will be the other number the algorithm is finding.
+a - t = b, b + a = t
+where a = ith element, t = target value, b = other number
+
+Why we store the (ith index, ith element)?
+We store the (ith index, ith element) so when the algorithm got the difference of the target value and
+ith element, we can find it easily in our hashmap since we stored our ith element in it and get also the
+ith index of that element since we only need to return the indices of the two numbers.
+"""
 class Solution(object):
     def twoSum(self, nums, target):
         # generate hashmap
