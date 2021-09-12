68b1ce68492f7f83f8fb3ec7acf88555223ec81c
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-09-13 01:43:47 +0800

    Time: 27 ms (24.97%), Space: 13.4 MB (65.46%) - LeetHub

---
+++ reverse-integer/reverse-integer.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def reverse(self, x):
+        isNeg = False
+        # checks if x is neg
+        if x < 0:
+            isNeg = True
+            # converts x to positive
+            x = x * -1
+        
+        # reverse num mathematically
+        rev_num = 0
+        while x > 0:
+            # gets the first digit
+            sgl_dig = x % 10
+            # appends to rev_num
+            rev_num = (rev_num * 10) + sgl_dig
+            # removes the last digit of x
+            x = x // 10
+            
+        # converts rev_num to neg if x is neg
+        if isNeg:
+            rev_num = rev_num * -1
+        
+        # returns rev_num if value is inside the signed 32-bit int range
+        # else returns 0
+        return rev_num if rev_num >= -2**31 and rev_num <= 2**31-1 else 0
