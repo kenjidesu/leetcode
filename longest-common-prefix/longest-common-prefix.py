f03e1421a9086236077a8d031dfb44cbdd68ddd2
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-09-13 03:00:12 +0800

    Time: 36 ms (13.86%), Space: 13.7 MB (34.89%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        # gets the shortest word in strs
+        shrtest = min(strs, key=len)
+        
+        # enumerate() = (index, indexValue)
+        for i, c in enumerate(shrtest):
+            # checks if strs is != to the indexValue of shrtest
+            for j in strs:
+                # returns the longest prefix if j is != indexValue
+                if j[i] != c:
+                    return shrtest[:i]
+                
+        return shrtest
