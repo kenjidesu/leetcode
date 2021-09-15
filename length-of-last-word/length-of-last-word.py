a072c9977cb5e255bc6d9a67194128df7214dea1
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-09-16 05:30:18 +0800

    Explanation added

--- length-of-last-word/length-of-last-word.py
+++ length-of-last-word/length-of-last-word.py
@@ -1,3 +1,11 @@
+"""
+split(), inputs a string value and outputs a list of words contained within 
+the string by separating or splitting the words on all the whitespaces by default.
+len(), used to calculate the length
+
+[-1] access the last index of the list of words of s, we can also use [len(spl)], but
+for simplicity we're gonna use [-1]
+"""
 class Solution(object):
     def lengthOfLastWord(self, s):
         # split the words, this becomes an array
