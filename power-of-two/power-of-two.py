c3d890b3e0bf7890015b62de01094997c2e74fe0
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-10-01 02:46:33 +0800

    Delete power-of-two.py

--- power-of-two/power-of-two.py
+++
@@ -1,9 +0,0 @@
-from math import log, ceil
-
-class Solution(object):
-    def isPowerOfTwo(self, n):
-        if n < 1:
-            return False
-        tl = math.log(n, 2)
-        g = float("{:.14f}".format(tl))
-        return math.ceil(g) == g
