b7a54b0573dcbb035cd035b7c80f07c0ca87b6a8
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-10-05 02:45:57 +0800

    Delete ransom-note.py

--- ransom-note/ransom-note.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        x = []
-        for i in set(ransomNote):
-            x.append(ransomNote.count(i) <= magazine.count(i))
-            
-        return all(x)
-            
