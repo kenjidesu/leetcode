3be5ae222cb5ff50b1bb1010fee5ba31b49541eb
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-10-07 04:01:56 +0800

    Comments Added

--- ugly-number/ugly-number.py
+++ ugly-number/ugly-number.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def isUgly(self, n):
+        # checks if positive
         if n < 1: return False
         
+        # Gets the lowest value of n using the given prime factors
         while n % 2 == 0: n /= 2
         while n % 3 == 0: n /= 3
         while n % 5 == 0: n /= 5
         
+        # If 1, that means it is divisible by this prime factors
         return n == 1
         
