b97ae160d607c8e7652854bec16bd9173919f3ad
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-10-12 11:00:51 +0800

    Update power-of-four.py

--- power-of-four/power-of-four.py
+++ power-of-four/power-of-four.py
@@ -1,8 +1,13 @@
 from math import log, ceil
 class Solution(object):
     def isPowerOfFour(self, n):
+        # Negative numbers are undefined in log()
         if n <= 0: return False
+        # gets the log of n base 4
         nlog = log(n, 4)
         
+        # ceil(), to make sure there's no decimal
+        # if there's decimal, ceil() rounds a number up to the next largest integer
+        # and that will make the ceil(nlog) != nlog
         return ceil(nlog) == nlog
         
