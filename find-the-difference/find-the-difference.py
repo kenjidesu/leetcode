d97b8c0436461fd0f0e42ba6a1874c9b61874a33
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-10-17 21:06:54 +0800

    Update find-the-difference.py

--- find-the-difference/find-the-difference.py
+++ find-the-difference/find-the-difference.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def findTheDifference(self, s, t):
+        # calculate the ord val of each strings
         nums = sum([ord(i) for i in s])
         numt = sum([ord(i) for i in t])
+        
+        # get the absolute difference of both strings
         return chr(abs(nums-numt))
         
