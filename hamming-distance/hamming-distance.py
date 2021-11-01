f7364d7e6bf3e2d28d70635a6e5e0fa3a5c8feb4
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-11-02 04:01:11 +0800

    Time: 8 ms (98.84%), Space: 13.2 MB (88.70%) - LeetHub

---
+++ hamming-distance/hamming-distance.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        # Get binary value
+        bx = bin(x)[2:]
+        by = bin(y)[2:]
+        
+        # Add 0 to match the value
+        ls = abs(len(bx) - len(by))
+        if ls:
+            dd = '0' * ls
+            if len(bx) > len(by):
+                by = dd + by
+            else:
+                bx = dd + bx
+        
+        # if xi != yi, adds 1
+        return sum(xi != yi for xi, yi in zip(bx, by))
+            
+                
+            
+        
+        
+        
