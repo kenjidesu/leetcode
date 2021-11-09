1f0025798c52ed00c3bb5bdd2a0dd65787b00fc0
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-11-09 21:17:18 +0800

    Delete single-number-ii.py

--- single-number-ii/single-number-ii.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        if len(nums) == 0: return
-        
-        for i in set(nums):
-            if nums.count(i) == 1:
-                return i
-        
-        return
