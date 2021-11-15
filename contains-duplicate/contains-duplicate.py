b819c3913009f7ccc25a74edc3334ba3645ada1b
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-11-15 11:30:31 +0800

    Time: 104 ms (48.94%), Space: 16.7 MB (95.17%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        # if there's a duplicate, set() will get rid of it
-        # and length of that will get affected
-        return len(nums) != len(set(nums))
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]: return True
+            
+        return False
