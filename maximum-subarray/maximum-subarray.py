215f8ee5aaea145cf1f57cfb20d033833f9f5a86
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-11-15 10:00:18 +0800

    Time: 636 ms (57.36%), Space: 25.7 MB (22.58%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,5 +1,10 @@
 class Solution(object):
     def maxSubArray(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 0:
+            return 0
+        
         # sets ng,ng = nums[0]
         ng = nc = nums[0]
         for i in range(1, len(nums)):
