9a26595db4213833341ff6c04b9dca4b67fe950c
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-11-26 20:55:39 +0800

    Delete search-a-2d-matrix.py

--- search-a-2d-matrix/search-a-2d-matrix.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        for i in range(len(matrix)):
-            if target in matrix[i] and matrix[i][0] <= target:
-                return True
-        
-        return False
-    
