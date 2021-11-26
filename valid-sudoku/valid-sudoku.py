5f4cd80184e112cf6dac84d040c97148fadf9adb
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-11-26 20:44:15 +0800

    Time: 84 ms (53.21%), Space: 13.4 MB (92.89%) - LeetHub

---
+++ valid-sudoku/valid-sudoku.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isValidSudoku(self, board):        
+        for i in range(9):
+            st1, st2 = set(), set()
+            for j in range(9):
+                if board[i][j].isnumeric():
+                    if board[i][j] in st1:
+                        return False
+                    st1.add(board[i][j])
+                    
+                if board[j][i].isnumeric():
+                    if board[j][i] in st2:
+                        return False
+                    st2.add(board[j][i])
+                    
+        for i in [0, 3, 6]:
+            for j in [0, 3, 6]:
+                st3 = set()
+                for k in range(3):
+                    for l in range(3):
+                        if board[i+k][j+l].isnumeric():
+                            if board[i+k][j+l] in st3:
+                                return False
+                            st3.add(board[i+k][j+l])
+                            
+        return True
