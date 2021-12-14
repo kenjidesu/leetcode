b9e5373d9a5fa58934bc5ce7ce336210a3895355
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-14 12:03:11 +0800

    Time: 620 ms (75.12%), Space: 25.5 MB (93.54%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,17 +1,10 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        if len(nums) == 1:
-            return nums[0]
-        elif len(nums) == 0:
-            return 0
-        
-        # sets ng,ng = nums[0]
-        ng = nc = nums[0]
+        mx = tmp = nums[0]
         for i in range(1, len(nums)):
-            # gets the max of i, i + nc
-            nc = max(nums[i], nums[i] + nc)
-            # updates ng if nc > ng, ng will be our max sub
-            if nc > ng:
-                ng = nc
-                
-        return ng
+            tmp = max(nums[i], nums[i] + tmp)
+            
+            if tmp > mx: mx = tmp
+            
+        return mx
+        
