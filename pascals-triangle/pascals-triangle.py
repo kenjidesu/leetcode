2a9cb3e5e77e11b39eb616048b22258c00c1181f
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-17 15:12:04 +0800

    Time: 15 ms (78.59%), Space: 13.3 MB (67.64%) - LeetHub

--- pascals-triangle/pascals-triangle.py
+++ pascals-triangle/pascals-triangle.py
@@ -1,12 +1,9 @@
 class Solution(object):
     def generate(self, numRows):
-        # Instantly creates arrays of 1 base on numRows
-        pascal = [[1]*(i+1) for i in range(numRows)]
+        pascal = [[1] * (i+1) for i in range(numRows)]
         
-        # Starts at 2 because the first 2 arrays are supposed to be 1's only
         for i in range(2, numRows):
             for j in range(1, i):
-                # adds the last array and set it to the mid of pascal
                 pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
-                
+        
         return pascal
