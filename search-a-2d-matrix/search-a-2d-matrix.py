046fed7c23a6d306f230f2b7955091dc05244cd7
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-18 19:52:47 +0800

    Time: 20 ms (99.10%), Space: 14 MB (21.23%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.py
+++ search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        for i in range(len(matrix)):
-            # only checks if the first element of matrix[i] is <= target
-            # since the matrix are sorted
-            if target in matrix[i] and matrix[i][0] <= target:
+        for i in matrix:
+            if target in i:
                 return True
-        
+            
         return False
-    
+        
