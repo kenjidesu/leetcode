8bcee3294b3b21bdc765e73d4285d5369a616050
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-20 11:02:14 +0800

    Time: 60 ms (81.47%), Space: 20.4 MB (31.54%) - LeetHub

--- remove-linked-list-elements/remove-linked-list-elements.py
+++ remove-linked-list-elements/remove-linked-list-elements.py
@@ -5,24 +5,21 @@
 #         self.next = next
 class Solution(object):
     def removeElements(self, head, val):
-        if head is None: return head
+        if not head:
+            return
         
-        prev = None
-        curr = head
-        while curr:
-            # we get rid of the first elements
-            # prev is None, it means that's the first element
-            if curr.val == val and prev is None:
-                head = curr.next
-                curr = curr.next
-            # we connect the prev node to the curr.next
-            # basically we're removing the val in the linked list
-            elif curr.val == val and prev is not None:
-                prev.next = curr.next
-                curr = prev.next
-            # update prev and curr
+        cur = head.next
+        prev = head
+        while cur:
+            if cur.val == val:
+                cur = cur.next
+                prev.next = cur
             else:
-                prev = curr
-                curr = curr.next
-                
+                prev = cur
+                cur = cur.next
+            
+        if head.val == val:
+            return head.next
+        
         return head
+        
