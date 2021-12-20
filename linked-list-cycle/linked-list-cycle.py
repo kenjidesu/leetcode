3a8e437ae3ff1e09666e6375f53ce7b6e6adb685
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-20 10:01:23 +0800

    Time: 40 ms (93.60%), Space: 20.3 MB (84.32%) - LeetHub

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -6,16 +6,12 @@
 
 class Solution(object):
     def hasCycle(self, head):
-        # Floyd's Algorithm
         slow, fast = head, head
-        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             
-            # if slow and fast meet up, therefore it has a cycle
             if slow == fast:
                 return True
-        
-        
+            
         return False
