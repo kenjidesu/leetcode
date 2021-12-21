e59c0a3849ccb171ea6056d2e343b74754abf5b2
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-21 10:50:12 +0800

    Time: 28 ms (48.20%), Space: 15.3 MB (80.12%) - LeetHub

--- reverse-linked-list/reverse-linked-list.py
+++ reverse-linked-list/reverse-linked-list.py
@@ -5,23 +5,14 @@
 #         self.next = next
 class Solution(object):
     def reverseList(self, head):
-        # checks if head exists
-        if head is None:
-            return head
+        cur = head
+        nxt = head
+        prev = None
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
         
-        curr = head
-        sec = head.next
-        
-        # reverse nodes
-        while curr and sec:
-            # connect to the next of sec
-            curr.next = sec.next
-            # connect to the head
-            sec.next = head
-            # set a new head
-            head = sec
-            # update sec
-            sec = curr.next
-        
+        head = prev
         return head
-        
