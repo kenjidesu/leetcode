7f3874b8ccaf264c9e1e4952ce6f62af649daf56
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-22 18:46:37 +0800

    Time: 8 ms (99.20%), Space: 13.4 MB (66.31%) - LeetHub

--- implement-queue-using-stacks/implement-queue-using-stacks.py
+++ implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -2,36 +2,23 @@ class MyQueue(object):
 
     def __init__(self):
         self.arr = []
-        
-    
+
     def push(self, x):
-        """
-        Pushes element x to the back of the queue
-        """
         self.arr.append(x)
         
 
     def pop(self):
-        """
-        Removes the element from the front of the queue and returns it
-        """
         if not self.empty():
             return self.arr.pop(0)
         
 
     def peek(self):
-        """
-        Returns the element at the front of the queue
-        """
         if not self.empty():
             return self.arr[0]
         
 
     def empty(self):
-        """
-         Returns true if the queue is empty, false otherwise
-        """
-        return False if self.arr else True
+        return len(self.arr) == 0
         
 
 
