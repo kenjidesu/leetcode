13ae17bee77c3cf04d1e98f3a2f7fb61f9bacd41
kenjidesu <70508103+kenjidesu@users.noreply.github.com>
2021-12-23 09:47:06 +0800

    Time: 16 ms (87.45%), Space: 13.7 MB (35.37%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,18 +1,15 @@
 class Solution(object):
     def isValid(self, s):
-        # stack, tracking the brackets
-        # opclo, dict of the same type of brackets
-        stack, opclo = [], {'}': '{', ']': '[', ')': '('}
+        brackets = {')': '(', '}': '{', ']': '['}
         
+        valid = []
         for i in s:
-            if i in opclo:
-                # returns False if the brackets of stack.pop() is not the same type
-                # or if stack is empty and it also pop() even tho its not the same type
-                if not (stack and stack.pop() == opclo[i]):
+            if i in brackets and len(valid) != 0:
+                if valid[-1] == brackets[i]:
+                    valid.pop()
+                else:
                     return False
             else:
-                stack.append(i)
-        
-        # returns True if stack is Empty
-        return not stack
-        
+                valid.append(i)
+                
+        return len(valid) == 0
